0241562cb10c5f7b7ae0d0689fdf82ad5812c37a
sherrychen2 <75269049+sherrychen2@users.noreply.github.com>
2022-04-27 22:32:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -78,36 +78,5 @@ var colors = new Array(
           });
 
 
-          document.addEventListener('DOMContentLoaded',function(event){
-            var dataText = [ "Hi, I'm Sherry Chen!", "I'm a Friend!", "I'm an Artist!", "I'm a Photographer!"];
-            
-            function typeWriter(text, i, fnCallback) {
-              if (i < (text.length)) {
-               document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-          
-                setTimeout(function() {
-                  typeWriter(text, i + 1, fnCallback)
-                }, 100);
-              }
-              else if (typeof fnCallback == 'function') {
-                setTimeout(fnCallback, 700);
-              }
-            }
-             function StartTextAnimation(i) {
-               if (typeof dataText[i] == 'undefined'){
-                  setTimeout(function() {
-                    StartTextAnimation(0);
-                  }, 20000);
-               }
-              if (i < dataText[i].length) {
-               typeWriter(dataText[i], 0, function(){
-                 StartTextAnimation(i + 1);
-               });
-              }
-            }
-            StartTextAnimation(0);
-          });
-
-
  
   
